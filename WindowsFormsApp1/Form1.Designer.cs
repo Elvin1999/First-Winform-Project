f602bbbd58a4af95dd34f3a1973eb37e84253630
camalzade_elvin <camalzade_elvin@outlook.com>
2019-01-08 21:02:09 +0400

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button = new System.Windows.Forms.Button();
+            this.nametextBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.surnametextBox = new System.Windows.Forms.TextBox();
+            this.agetextBox = new System.Windows.Forms.TextBox();
+            this.maleradioButton = new System.Windows.Forms.RadioButton();
+            this.groupBox = new System.Windows.Forms.GroupBox();
+            this.femaleradioButton2 = new System.Windows.Forms.RadioButton();
+            this.groupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button
+            // 
+            this.button.Location = new System.Drawing.Point(426, 196);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(61, 25);
+            this.button.TabIndex = 0;
+            this.button.Text = "button1";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // nametextBox1
+            // 
+            this.nametextBox1.Location = new System.Drawing.Point(213, 91);
+            this.nametextBox1.Name = "nametextBox1";
+            this.nametextBox1.Size = new System.Drawing.Size(101, 20);
+            this.nametextBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(213, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(213, 153);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Age";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(213, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Surname";
+            // 
+            // surnametextBox
+            // 
+            this.surnametextBox.Location = new System.Drawing.Point(213, 130);
+            this.surnametextBox.Name = "surnametextBox";
+            this.surnametextBox.Size = new System.Drawing.Size(101, 20);
+            this.surnametextBox.TabIndex = 5;
+            // 
+            // agetextBox
+            // 
+            this.agetextBox.Location = new System.Drawing.Point(213, 169);
+            this.agetextBox.Name = "agetextBox";
+            this.agetextBox.Size = new System.Drawing.Size(101, 20);
+            this.agetextBox.TabIndex = 6;
+            // 
+            // maleradioButton
+            // 
+            this.maleradioButton.AutoSize = true;
+            this.maleradioButton.Location = new System.Drawing.Point(6, 31);
+            this.maleradioButton.Name = "maleradioButton";
+            this.maleradioButton.Size = new System.Drawing.Size(47, 17);
+            this.maleradioButton.TabIndex = 8;
+            this.maleradioButton.TabStop = true;
+            this.maleradioButton.Text = "male";
+            this.maleradioButton.UseVisualStyleBackColor = true;
+            // 
+            // groupBox
+            // 
+            this.groupBox.Controls.Add(this.femaleradioButton2);
+            this.groupBox.Controls.Add(this.maleradioButton);
+            this.groupBox.Location = new System.Drawing.Point(510, 39);
+            this.groupBox.Name = "groupBox";
+            this.groupBox.Size = new System.Drawing.Size(156, 111);
+            this.groupBox.TabIndex = 9;
+            this.groupBox.TabStop = false;
+            this.groupBox.Text = "Gender";
+            // 
+            // femaleradioButton2
+            // 
+            this.femaleradioButton2.AutoSize = true;
+            this.femaleradioButton2.Location = new System.Drawing.Point(6, 55);
+            this.femaleradioButton2.Name = "femaleradioButton2";
+            this.femaleradioButton2.Size = new System.Drawing.Size(56, 17);
+            this.femaleradioButton2.TabIndex = 9;
+            this.femaleradioButton2.TabStop = true;
+            this.femaleradioButton2.Text = "female";
+            this.femaleradioButton2.UseVisualStyleBackColor = true;
+            // 
+            // form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox);
+            this.Controls.Add(this.agetextBox);
+            this.Controls.Add(this.surnametextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nametextBox1);
+            this.Controls.Add(this.button);
+            this.Name = "form1";
+            this.Text = "Elvin\'s App";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox.ResumeLayout(false);
+            this.groupBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.TextBox nametextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox surnametextBox;
+        private System.Windows.Forms.TextBox agetextBox;
+        private System.Windows.Forms.RadioButton maleradioButton;
+        private System.Windows.Forms.GroupBox groupBox;
+        private System.Windows.Forms.RadioButton femaleradioButton2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace WindowsFormsApp1
+{
+    public partial class form1 : Form
+    {
+        public User user { get; set; }
+        public form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BackColor = Color.Red;
+        }
+
+    
+        private void button_Click(object sender, EventArgs e)
+        {
+            user = new User();
+            Random r = new Random();
+            button.Left = r.Next(0, Width/2);
+            button.Top = r.Next(0, Height/2);
+            BackColor = Color.FromArgb(r.Next(0, 256), r.Next(0, 256), 0);
+            var text = agetextBox.Text;
+            user.Age = Convert.ToInt32(text);
+            user.Name = nametextBox1.Text;
+            user.Surname = surnametextBox.Text;
+            var item = maleradioButton;
+            string gender = string.Empty;
+            foreach (var g in groupBox.Controls)
+            {
+                var casted = g as RadioButton;
+                if (casted.Checked)
+                {
+                    gender = casted.Text;
+                }
+            }
+            MessageBox.Show($"{user.Surname} {user.Name} {user.Age} {gender}");
+        }
+
+
+    }
+}
